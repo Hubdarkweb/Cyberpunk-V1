ac68419f23c57ef809c1d65b88cf5ac6e428e926
Hubdarkweb <klenin980@gmail.com>
2024-09-03 15:37:10 +0300

    Update parole.js

--- commandes/parole.js
+++ commandes/parole.js
@@ -18,7 +18,7 @@ zokou({ nomCom: "lyrics",
 
     if (lyrics.error) return repondre("no lyrics found");
 
-    let msg = `---------*Gojou-md*--------
+    let msg = `---------*Cyberpunk-V1*--------
 
 * *Artist :* ${lyrics.artist}
 
