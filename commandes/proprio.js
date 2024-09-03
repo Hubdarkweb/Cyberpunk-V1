8a4c046ecb6637f29e4dec3629e6b6af033ac9e9
Hubdarkweb <klenin980@gmail.com>
2024-09-03 15:37:48 +0300

    Update proprio.js

--- commandes/proprio.js
+++ commandes/proprio.js
@@ -14,7 +14,7 @@ const{repondre,ms,dev,superUser}=com;
 
   if(!superUser)
   {
-    return repondre("This command is for *Gojou-md* owner only");
+    return repondre("This command is for *Cyberpunk-V1* owner only");
   }
 
   const {exec}=require("child_process")
